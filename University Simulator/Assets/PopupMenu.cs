82b1fdbadd326361f8cb3c7ce3417ad123f39a60
Max Ainatchi <maxwell.ainatchi@gmail.com>
2019-04-29 15:21:38 -0400

    +sample scrolling UI

---
+++ University Simulator/Assets/PopupMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PopupMenu: MonoBehaviour, ClickableTileListener
+{
+    public Tilemap map;
+    public GameObject menuObject;
+
+    // Update is called once per frame
+    
+    public void DidClickTile(Vector3 worldPosition, Tilemap tilemap, bool isValid) {
+        if (isValid) {
+            this.menuObject.SetActive(true);
+            this.menuObject.transform.position = worldPosition;
+        } else {
+            this.menuObject.SetActive(false);
+        }
+    }
+
+    void OnClickMenuItem() {
+
+    }
+}
---
+++ University Simulator/Assets/Scrollable.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Scrollable : MonoBehaviour
+{  
+    public float speed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            this.gameObject.transform.Translate(new Vector3(0, -1 * speed * Time.deltaTime, 0));
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            this.gameObject.transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            this.gameObject.transform.Translate(new Vector3(-1 * speed * Time.deltaTime, 0, 0));
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            this.gameObject.transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+    }
+}
