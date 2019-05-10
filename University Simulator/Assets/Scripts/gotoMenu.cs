03c146d6a1a6554d1a8fcdb2b63a16b43746cfa2
Jahlil Walker <jgw298@nyu.edu>
2019-05-10 03:03:13 -0400

    Added Tutorial Scene

---
+++ University Simulator/Assets/Scripts/gotoMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gotoMenu : MonoBehaviour
+{
+    public void loadMenu(){
+        SceneManager.LoadScene(0);
+        
+    } 
+}
