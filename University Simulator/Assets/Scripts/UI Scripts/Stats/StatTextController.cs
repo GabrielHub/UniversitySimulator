0a1850f600684d419ab54024ecf82e24f7b08c53
Max Ainatchi <maxwell.ainatchi@gmail.com>
2019-05-13 01:38:58 -0400

    moved abbreviated text extension

--- University Simulator/Assets/Scripts/UI Scripts/Stats/StatTextController.cs
+++ University Simulator/Assets/Scripts/UI Scripts/Stats/StatTextController.cs
@@ -3,32 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public static class CustomNumString {
-    public static string ToAbbreviatedString(this int value, int decimals = 1) {
-        if (value < 1_000) return value.ToString();
-        return ((float) value).ToAbbreviatedString(decimals);
-    }
-
-    public static string ToAbbreviatedString(this float value, int decimals = 1) {
-        string unit = "";
-        float denom = 1;
-        if (value >= 1_000_000_000) {
-            unit = "b";
-            denom = 1_000_000_000;
-        } else if (value >= 1_000_000) {
-            unit = "m";
-            denom = 1_000_000;
-        } else if (value >= 1_000) {
-            unit = "k";
-            denom = 1_000;
-        } else {
-            return value.ToString();
-        }
-        float precision = Mathf.Pow(10f, decimals);
-        return Mathf.Round(value / denom * precision) / precision  + unit;
-    }
-}
-
 public class StatTextController : MonoBehaviour {
     public StatController stat;
     private TextMeshProUGUI text;
